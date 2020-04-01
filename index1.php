80ccd6e79cb7df76e90681b7c617d30869989d5c
lavanyasaini1600 <lavanyasaini1600@gmail.com>
2020-04-01 18:39:07 -0400

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>COMP1006 Music Library</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md bg-navy navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">Customer Form</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/persons-list.php">View All</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/person-create.php">Add New</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            
+                <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>                
+          
+
+            <li class="nav-item">
+                
+                <a class="nav-link" href="/login.php">Log In</a>            </li>
+            <li class="nav-item">
+            
+        </ul>
+    </div>
+</nav>
+
+
+<h1>This is the Insurance Management System and using crud operation for persons/customers details.</h1>
+<p>Insurance has become an integral part of virtually everything we do in life and business. Roles within the insurance industry are many and varied. Whether you enjoy working with numbers, doing research, solving problems or working with people, you can find your specialty in insurance.
+
+Humber’s Insurance Management – Property and Casualty graduate certificate program prepares you for a variety of entry level management career paths in the large and growing property and casualty insurance industry. Learn the fundamentals of the industry and its three main functional areas – underwriting, loss adjusting and agency/brokerage – and gain transferable skills in business communications, computer applications, insurance accounting and metrics, marketing, sales and customer service, project management and leadership. Employment-focused experiential learning helps you consider a variety of options, as you launch your career.</p>
+<p>Source code available at <a href="https://github.com/lavanyasaini1600/assignment-2-">https://github.com/lavanyasaini1600/assignment-2</a>.</p>
+
+<!-- Footer from https://mdbootstrap.com/docs/jquery/navigation/footer/ -->
+<footer class="page-footer font-small bg-footer">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">
+        &copy; 2020 Copyright!
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<!-- include our custom js library -->
+<script src="js/scripts.js" type="text/javascript"></script>
+
+</body>
+</html>
